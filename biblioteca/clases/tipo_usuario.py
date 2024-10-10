19559551a55186b69459919f8c99bfa56e7ba526
Marcelozurita1 <joaquinzrt15@gmail.com>
2024-10-10 20:17:55 -0300

    CAMBIOS

---
+++ biblioteca/clases/tipo_usuario.py
@@ -0,0 +1,6 @@
+class Tipo_Usuario():  # Define una clase llamada 'Tipo_Usuario'
+    def __init__(self, id_tipo_usuario, tipo_usuario, descripcion_tipo_usuario):
+        # Este es el constructor que se ejecuta al crear una nueva instancia de la clase
+        self.id_tipo_usuario = id_tipo_usuario  # Almacena el ID del tipo de usuario
+        self.tipo_usuario = tipo_usuario  # Almacena el nombre del tipo de usuario
+        self.descripcion_tipo_usuario = descripcion_tipo_usuario  # Almacena una descripción del tipo de usuario
